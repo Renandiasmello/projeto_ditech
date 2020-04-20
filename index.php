aa46ed9fd9c728ad1c4152bfbf51f5b813f80a0f
Renan Mello <renandiasmello182@gmail.com>
2020-04-19 23:33:05 -0300

    Retirado comentario do codigo.

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
-<!--<form method="post" action="ope.php" id="formlogin" name="formlogin">-->
-<!--    <fieldset>-->
-<!--        <legend>Login</legend>-->
-<!--        <br/>-->
-<!--        <label>Login : </label>-->
-<!--        <input type="text" name="login" id="login"/><br/>-->
-<!--        <label>Senha :</label>-->
-<!--        <input type="password" name="senha" id="senha"/><br/>-->
-<!--        <input type="submit" value="Entrar"/>-->
-<!--    </fieldset>-->
-<!--</form>-->
-<?php define('DS', DIRECTORY_SEPARATOR); ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Sign in Form with Facebook and Twitter Buttons</title>
+    <title>Login [Projeto Ditech]</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
